ecff432471e6b9afb54af2823b4ae71cec046661
Riza Rifantara <109719996+Phoenixcat01@users.noreply.github.com>
2022-09-21 23:21:02 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        'default': '#6FD067',
+        'defaultgrey': '#B9CBCB',
+        'defaultcoco': '#5D4A4A',
+        'defaultyellow': '#E9F36D',
+      },
+    },
+  },
+  plugins: [],
+}
